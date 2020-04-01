b40499dd351cecc9e930f6ab0474ce0a611f9556
D D <ddeljanin5@icloud.com>
2020-04-01 22:11:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,8 @@ def valid_move?(board, index)
   # and is the position unoccupied?
   if index.between(0,8) && position_taken(board, index)
     true
-  
+  else
+    false
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
