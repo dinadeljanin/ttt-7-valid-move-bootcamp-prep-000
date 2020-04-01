f73e667f96be30239301d6931295e7e174eb6703
D D <ddeljanin5@icloud.com>
2020-04-01 22:20:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,12 +2,11 @@
 def valid_move?(board, index)
   # did the user choose a cell that has an index of 0-8?
   # and is the position unoccupied?
-  # if index.between?(0,8) && position_taken?(board, index) == false
-  #   true
-  # else
-  #   false
-  # end
-  index.between?(0,8) && position_taken?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
