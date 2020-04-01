759ab8a8a26496da686a3a67ef036d8ccf6ac5fb
D D <ddeljanin5@icloud.com>
2020-04-01 22:21:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,8 +11,10 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
+  # empty string means free
   if board[index] == " "
     false
+  # 
   elsif board[index] == ""
     false
   elsif board[index] == nil
